a718f8f6e8a0c2f3163d998079dd1d96213f7baa
Elfat M <104862141+elfat-py@users.noreply.github.com>
2022-12-19 19:07:51 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<cmath>
+#include <string>
+using namespace std;
+
+/* ***************|***************
+   ***************|***************
+   ***************|***************
+   *******2*******|*******1*******
+   ***************+***************
+   ***************|***************
+------------(-)-------+------------->
+   ***************|***************
+   ***************|***************
+   *******3*******-*******4*******
+   ***************|***************
+   ***************|***************
+   ***************|***************
+*/
+
+int main (){
+
+    int xPoint, yPoint;
+
+    cout<<"Enter the value of the X coordinate(x-axis) ";
+    cin>>xPoint;
+
+    cout<<"Enter the value of the Y coordinate(y-axis) ";
+    cin>>yPoint;
+
+    if (xPoint>0 && yPoint>0)
+    {
+        cout<<"This point is in the first quarter. ";
+
+    }else if(xPoint>0 && yPoint<0)
+    {
+        cout<<"This point is in the first quarter. ";
+
+    }else if (xPoint<0 && yPoint>0)
+    {
+        cout<<"This point is in the second quarter. ";
+
+    }else if (xPoint<0 && yPoint<0)
+    {
+        cout<<"This point is in the third quarter. ";
+
+    }else if (xPoint==0 && yPoint==0)
+    {
+        cout<<"This point is in the fourth quarter. ";
+
+    }else{
+        cout<<"There must be a problem with the coordinates!";
+    }
+     
+    return 0;
+}
